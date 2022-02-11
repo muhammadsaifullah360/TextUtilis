fcef705bd603a2b8a15c3025864ae090fa2d45b4
Muhammad Saif Ullah <miansaif360@gmail.com>
2022-02-11 18:07:02 +0500

    keyChange commit by admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_iq6q1)ubmkic*x%bi(v33mdgw#^&x)4%$x^31st2$0sy0o7*8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
